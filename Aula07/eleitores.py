574f000e361d3e8dc84bf03151d403d775dd8828
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2022-12-16 09:28:41 -0300

    Create eleitores.py

---
+++ Aula07/eleitores.py
@@ -0,0 +1,15 @@
+#Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. 
+# Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+
+João = []
+Tereza =[]
+José = []
+
+quant_eleitores = int(input("Digite quantidade de votos"))
+print("Candidatos: João número 1, Tereza número 2, José número 3")
+
+while range(quant_eleitores):
+    voto = int(input("Digite o numero do candidato:"))
+    if voto == 1 :
+        voto.append[João]
+    print(João)
