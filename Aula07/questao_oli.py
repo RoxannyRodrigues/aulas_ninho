c86f2193a0ce5d1548d057d71fc0578c45ebfc3f
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2022-12-16 09:27:08 -0300

    Create questao_oli.py

---
+++ Aula07/questao_oli.py
@@ -0,0 +1,12 @@
+alunos = list(range(20))
+
+numeroDeEquipes = int(input("Insira o número de equipe: "))
+equipe = []
+
+while(len (alunos)> 0):
+
+    if (len(equipe)!= numeroDeEquipes):
+        for i in range(numeroDeEquipes):
+            equipe.append([])
+        
+                                    
