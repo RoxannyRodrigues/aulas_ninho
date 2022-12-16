d4982a59b986bb9eb7c10f5f2647439d40724306
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2022-12-16 09:29:06 -0300

    Create algo_2.py

---
+++ Aula07/algo_2.py
@@ -0,0 +1,13 @@
+def helloWorld():
+    print("Olá, ")
+
+def hello(meu_nome, minha_idade):
+    print("Olá", meu_nome, "\nSua idade é: ", minha_idade)
+
+helloWorld()    
+
+nome = (input("Digite seu nome: "))
+idade = (input("Digite sua idade: "))
+
+hello(nome, idade)
+  
